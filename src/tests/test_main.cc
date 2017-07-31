d0fbef5300d3a369dd6138c38fe66e9447398e16
shashu96 <shashankjoisa@gmail.com>
2017-07-31 11:30:42 +0530

    Added testing for KF tracking

--- src/tests/test_main.cc
+++ src/tests/test_main.cc
@@ -128,6 +128,7 @@ DECLARE_string(log_dir);
 #include "unit-tests/signal-processing-blocks/tracking/gps_l2_m_dll_pll_tracking_test.cc"
 #if MODERN_ARMADILLO
 #include "unit-tests/signal-processing-blocks/tracking/gps_l1_ca_dll_pll_tracking_test.cc"
+#include "unit-tests/signal-processing-blocks/tracking/gps_l1_ca_dll_kf_tracking_test.cc"
 #include "unit-tests/signal-processing-blocks/telemetry_decoder/gps_l1_ca_telemetry_decoder_test.cc"
 #endif
 #endif
